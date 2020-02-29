5d9bf6debce3331dc51da157dee5efc197da83ad
WMXPY <wm@mengw.io>
2020-02-29 15:15:05 -0600

    implement merge content function

--- lib/src/prettify.dart
+++ lib/src/prettify.dart
@@ -1,5 +1,24 @@
 import 'package:logo/logo.dart';
 
+class PrettifyConfig {
+  final bool showTime;
+
+  PrettifyConfig({
+    this.showTime = false,
+  });
+}
+
+String mergeContent(String quote, String content, PrettifyConfig config) {
+  if (config.showTime) {
+    final DateTime dateTime = DateTime.now();
+    final String prettifiedDate = appropriateDateStringWithTime(dateTime);
+
+    return "$quote $prettifiedDate $content";
+  }
+
+  return "$quote $content";
+}
+
 String getLogQuote(String level) {
   switch (level) {
     case LogoLogLevelType.VERBOSE:
