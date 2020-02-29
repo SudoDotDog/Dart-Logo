e64090f0b8fa40e4cc3d096f450b75e8713bb9f7
WMXPY <wm@mengw.io>
2020-02-29 14:30:06 -0600

    init unut tests

---
+++ test/declare_test.dart
@@ -0,0 +1,12 @@
+import 'package:logo/logo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Log Level Declare', () {
+    test('get alias can return word', () {
+      final String verbose = LogoLogLevel.VERBOSE;
+
+      expect(verbose, verbose);
+    });
+  });
+}
